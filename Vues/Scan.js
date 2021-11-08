f8325bba96c843159f4354d3699ab0889b52cc7c
Hagarde <43098624+Hagarde@users.noreply.github.com>
2021-11-08 10:59:43 +0100

    first step to scan

--- Vues/Scan.js
+++ Vues/Scan.js
@@ -1,83 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React,{useState, useEffect} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { BarCodeScanner } from 'expo-barcode-scanner';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
 
-export default function Scan() {
+export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
-  const [scanned, setScanned] = useState(false);
-  const [text,setText] = useState('Please scan your fucking product ! ')
+  const [type, setType] = useState(Camera.Constants.Type.back);
 
-  const askForCameraPermission = () => {
+  useEffect(() => {
     (async () => {
-      const {status} = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission( status === 'granted' );
-    })()
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+
+  onBarCodeScanned = (type, code) => {
+    console.log('le type du barcode est : ' + type);
+    console.log('le data du barcode est : ' + code);
+
+    // this.props.navigation.navigate('Matériau',{});
   }
-
-  _handleBarcode= (barcode) => {
-    this.props.navigation.navigate('Scan');
+  if (hasPermission === null) {
+    return <View />;
   }
-
-   // Request permission 
-  
-  useEffect( () => {
-    askForCameraPermission();
-  },[])
-
-  // What happen when a barcode is scanned
-  
-  const handleBarcodeScanned = ({type,data}) => {
-    setScanned(true);
-    setText(data);
-    console.log('Type : ' + type + ' / code-barre : ' + data);
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
   }
-
-  // pendant la demande de permission 
-  if (hasPermission == null) {
-    return (
-    <View style={styles.container}>
-      <Text>Bienvenue sur RecyScan </Text>
-      <StatusBar style="auto" />
-      <Text > Get permission in progress</Text>
-    </View>
-    );
-  }
-
-
-  // En cas de refus de permission rendre cette vue 
-  if (hasPermission == false ) {
-    return(
-    <View style={styles.container}>
-      <Text > Pk t'as refusé, batard, vas-y donne les accès ! </Text>
-      <Button title='get-perm' onPress={() => askForCameraPermission()}> Donner toute ta thune </Button>
-    </View>
-    );
-  }
-
-
-  // en cas de pas les cas précédent 
-
-
   return (
-    <View >
-      <View style={styles.container}>
-        <Text>Bienvenue sur RecyScan </Text>
-        <StatusBar style="auto" />
-        <BarCodeScanner onBarCodeScanned={scanned ? undefined : handleBarcodeScanned}/>
-      </View>
-      <Text> {text} </Text>
-
-      {scanned && <Button title='Scan un produit ! ' onClick={() =>{ setScanned(false);}} color="primary" />}
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type} onBarCodeScanned={onBarCodeScanned}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+styles = StyleSheet.create({
+  container: { 
+    width: '100%',
+    height: '100%',
   },
-});
+  camera: {
+    width: '100%',
+    height: '100%',
+  },
+  button: {
+
+  },
+  text: { 
+    textDecorationColor: 'rgba(255, 255, 255,0.8)'
+  }
+})
