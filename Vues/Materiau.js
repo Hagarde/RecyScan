508bf7bcfee17cae10ea4204a3c113caabfc09e1
Hagarde <43098624+Hagarde@users.noreply.github.com>
2021-11-29 16:59:47 +0100

    v1.0

--- Vues/Materiau.js
+++ Vues/Materiau.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Button } from 'react-native';
-import { StyleSheet, Text, View,TouchableOpacity,Image,useWindowDimensions,TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity,Image,Dimensions,TouchableHighlight } from 'react-native';
 import { traducteur } from '../Component/travail'
 import axios from 'axios';
 import * as Location from 'expo-location';
@@ -49,39 +49,58 @@ export default class Materiau extends React.Component  {
     if (this.state.trouve & !(!Array.isArray(this.state.packaging) || !this.state.packaging.length) ) {
       return ( 
         <View style={ styles.wrapper}>
-
-          <Text> Nous avons trouvé que cet emballage contenais des traces de : </Text>
-          {(this.state.packaging.includes('Plastique')) && 
-          <TouchableHighlight style={[{flexDirection:'raw'}, styles.wrapper2]} onPress={this._cparou('Plastique')}>
-            <Image style={styles.buttonLogo} source={require('../Styles/Image/plastique.png')}/>
-            <Text style={styles.buttonText}> Plastique </Text>
-          </TouchableHighlight>}
-          {(this.state.packaging.includes('Metal')) && 
-          <TouchableHighlight style={[{flexDirection:'raw'}, styles.wrapper2]} onPress={this._cparou('Metal')}>
-            <Image style={styles.buttonLogo} source={require('../Styles/Image/aluminium.png')}/>
-            <Text style={styles.buttonText}> Metal </Text>
-          </TouchableHighlight>
-          }
-          {(this.state.packaging.includes('Carton')) && 
-          <TouchableHighlight style={[{flexDirection:'raw'}, styles.wrapper2]} onPress={this._cparou('Carton')}>
-            <Image style={styles.buttonLogo} source={require('../Styles/Image/CartonGrand.png')}/>
-            <Text style={styles.buttonText}> Carton </Text>
-          </TouchableHighlight>}
-          {(this.state.packaging.includes('Brique')) && 
-          <TouchableHighlight style={[{flexDirection:'raw'}, styles.wrapper2]} onPress={this._cparou('Brique')}>
-            <Image style={styles.buttonLogo} source={require('../Styles/Image/brique.png')}/>
-            <Text style={styles.buttonText}> Brique </Text>
-          </TouchableHighlight>}
-          {(this.state.packaging.includes('Verre')) && 
-          <TouchableHighlight style={[{flexDirection:'raw'}, styles.wrapper2]} onPress={this._cparou('Verre')}>
-            <Image style={styles.buttonLogo} source={require('../Styles/Image/verre.png')}/>
-            <Text style={styles.buttonText}> Verre </Text>
-          </TouchableHighlight>}
-          {(this.state.packaging.includes('Aluminium')) && 
-          <TouchableHighlight style={[{flexDirection:'raw'}, styles.wrapper2]} onPress={this._cparou('Aluminium')}>
-            <Image style={styles.buttonLogo} source={require('../Styles/Image/aluminium.png')}/>
-            <Text style={styles.buttonText}> Aluminium </Text>
-          </TouchableHighlight>}
+          
+          <Text style={styles.welcomeText}> Nous avons trouvé que cet emballage contenait des traces de : </Text>
+          
+          <View style={ styles.wrapper }>
+            {(this.state.packaging.includes('Plastique')) && 
+            <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={()=>this._cparou('Plastique')}>
+              <View>
+                <Image style={styles.buttonLogo} source={require('../Styles/Image/plastique.png')}/>
+                <Text style={styles.buttonText}> Plastique </Text>
+              </View>
+            </TouchableHighlight>}
+            {(this.state.packaging.includes('Metal')) && 
+            <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={()=>this._cparou('Metal')}>
+              <View>
+                <Image style={styles.buttonLogo} source={require('../Styles/Image/aluminium.png')}/>
+                <Text style={styles.buttonText}> Metal </Text>
+              </View>
+            </TouchableHighlight>
+            }
+            {(this.state.packaging.includes('Carton')) && 
+            <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={()=>this._cparou('Carton')}>
+              <View>
+                <Image style={styles.buttonLogo} source={require('../Styles/Image/CartonGrand.png')}/>
+                <Text style={styles.buttonText}> Carton </Text>
+              </View>
+            </TouchableHighlight>}
+            {(this.state.packaging.includes('Brique')) && 
+            <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={()=>this._cparou('Brique')}>
+              <View>
+                <Image style={styles.buttonLogo} source={require('../Styles/Image/brique.png')}/>
+                <Text style={styles.buttonText}> Brique </Text>
+                </View>
+            </TouchableHighlight>}
+            {(this.state.packaging.includes('Verre')) && 
+            <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={()=>this._cparou('Verre')}>
+              <View>
+                <Image style={styles.buttonLogo} source={require('../Styles/Image/verre.png')}/>
+                <Text style={styles.buttonText}> Verre </Text>
+              </View>
+            </TouchableHighlight>}
+            {(this.state.packaging.includes('Aluminium')) && 
+            <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={()=>this._cparou('Aluminium')}>
+              <View>
+                <Image style={styles.buttonLogo} source={require('../Styles/Image/aluminium.png')}/>
+                <Text style={styles.buttonText}> Aluminium </Text>
+              </View>
+            </TouchableHighlight>}
+            <View style={styles.wrapper }>
+              <Text style={[{padding:20},styles.buttonText]}> Cliquez pour décourvrir où le recycler ! </Text>
+            </View>
+          </View>
+          
         </View>
       )
     }
@@ -90,21 +109,21 @@ export default class Materiau extends React.Component  {
       // Vue retourner en cas de non trouvage des matériaux
         <View style={ styles.wrapper }>
           <Text style={styles.welcomeText}> Aucune correspondance dans nos base de données, d'après vous de quoi est fait l'emballage que vous voulez recycler :  </Text>
-          <View style={ styles.wrapper }>
+          <View style={[{flexDirection:'row'},styles.wrapper] }>
             <View style={ styles.wrapper }>
-              <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={this._cparou('Plastique')}>
+              <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={()=> this._cparou('Plastique')}>
                 <View>
                   <Image style={styles.buttonLogo} source={require('../Styles/Image/plastique.png')}/>
                   <Text style={styles.buttonText}> Plastique </Text>
                 </View>
               </TouchableHighlight>  
-              <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={this._cparou('Carton')}>
+              <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={()=>this._cparou('Carton')}>
                 <View>
                   <Image style={styles.buttonLogo} source={require('../Styles/Image/CartonGrand.png')}/>
                   <Text style={styles.buttonText}> Carton </Text>
                 </View>
               </TouchableHighlight>
-              <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={this._cparou('Metal')}>
+              <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={()=>this._cparou('Metal')}>
                 <View>
                   <Image style={styles.buttonLogo} source={require('../Styles/Image/aluminium.png')}/>
                   <Text style={styles.buttonText}> Metal </Text>
@@ -112,19 +131,19 @@ export default class Materiau extends React.Component  {
               </TouchableHighlight>
             </View>
             <View style={ styles.wrapper }>
-              <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={this._cparou('Verre')}>
+              <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={()=>this._cparou('Verre')}>
                 <View>
                   <Image style={styles.buttonLogo} source={require('../Styles/Image/verre.png')}/>
                   <Text style={styles.buttonText}> Verre </Text>
                 </View>
               </TouchableHighlight> 
-              <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={this._cparou('Brique')}>
+              <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={()=>this._cparou('Brique')}>
                 <View>
                   <Image style={styles.buttonLogo} source={require('../Styles/Image/brique.png')}/>
                   <Text style={styles.buttonText}> Brique </Text>
                 </View>
               </TouchableHighlight>
-              <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={this._cparou('Aluminium')}>
+              <TouchableHighlight style={[{flexDirection:'row'}, styles.wrapper2]} onPress={()=>this._cparou('Aluminium')}>
                 <View> 
                   <Image style={styles.buttonLogo} source={require('../Styles/Image/aluminium.png')}/>
                   <Text style={styles.buttonText}> Aluminium </Text>
@@ -169,13 +188,17 @@ export default class Materiau extends React.Component  {
       padding: 10
     },
     wrapper2: {
-      padding: 15,
+      margin: 15,
+      padding: 10,
       display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
       borderRadius: 10,
       borderWidth: 1,
       borderColor: colors.white,
       backgroundColor: colors.white, 
-      width: useWindowDimensions().width * 0.4
+      width: Dimensions.get('window').width * 0.4,
+      height: Dimensions.get('window').height * 0.15
     },
     buttonText: {
       fontSize: 16,
